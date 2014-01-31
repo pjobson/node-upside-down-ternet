1a5b6c562ba54b838fe5c735b9d6312cd853709f
Paul Jobson <pjobson@gmail.com>
2014-01-31 03:37:55 -0500

    code cleanup

--- upside-down-ternet.node.js
+++ upside-down-ternet.node.js
@@ -1,52 +1,102 @@
 (function () {
     'use strict';
 
-    var http = require("http"),
-        url = require("url"),
-        path = require("path"),
-        fs = require("fs"),
-        port = process.argv[2] || 8888,
-        magick = require('imagemagick');
+    var upside_down_ternet = function() {
+        var that = this;
+        this.http    =require("http");
+        this.server  =this.http.createServer();
+        this.url     =require("url");
+        this.path    =require("path");
+        this.fs      =require("fs");
+        this.port    =8888;
+        this.magick  =require('imagemagick');
+        this.md5     =require('MD5');
+        this.request =null;
+        this.response=null;
+        this.resImg  =null;
+        this.img     = {
+            path: '/Users/pjobson/bin/img/',
+            src:  null,
+            md5:  null,
+            tmp:  null,
+            new:  null,
+            data: '',
+            contentType: null
+        };
+        
+        this.server.listen(parseInt(this.port, 10));
+        
+        this.server.on('connection', function (request, response) {
+            void(0);
+        });
+        
+        this.server.on('request', function (request, response) {
+            console.log('request');
+            that.response = response;
+            that.request  = request;
+            that.setImgParams();
+            return;
+        });
+        
+        this.setImgParams = function() {
+            that.img.src  = that.request.url.match(/q=(.+)/)[1];
+            that.img.md5  = that.md5(that.img.src);
+            that.img.tmp  = that.img.path+'tmp';
+            that.img.new  = that.img.path+that.img.md5;
+            that.img.data = '';
+            that.img.contentType = '';
 
-    var server = http.createServer();
-    var responseMain,response,imgData;
-    var imgPath = '/Users/pjobson/bin/img/';
-    var date = new Date();
-
-    server.on('connection', function (request, response) {
-        void(0); 
-    });
-
-    server.on('request', function(request, responseMain) {
-        var imgSRC = request.url.match(/q=(.+)/)[1];
-        var imgReq = http.get(imgSRC, function(response) {         
-            response.setEncoding('binary');
-            imgData='';
-    
-            response.on('data', function(chunk){
-                imgData += chunk;
+            that.imageCheck();
+            return true;
+        };
+        
+        this.imageCheck = function() {
+            that.fs.stat(that.img.new,function(err,data) {
+                if (data) {
+                    that.serveImage();
+                    return true;
+                } else {
+                    that.getImage();
+                    return true;
+                }
             });
-
-            response.on('end',function() {
-                var ext = response.headers['content-type'].replace(/.+\//,'.');
-                var ts  = date.getTime()+'';
-                var newImg = imgPath+ts+ext;
-                var contentType = response.headers['content-type'];
-                fs.writeFile(newImg, imgData, {encoding: 'binary'});
-    
-                magick.convert([newImg, '-flip', imgPath+'tmp'], function(err, stdout){
-                    if (err) throw err;
-                    fs.rename(imgPath+'tmp',newImg, function() {
-                        fs.readFile(newImg, function(err,data) {
-                            responseMain.writeHead(200, {"Content-Type": contentType});
-                            responseMain.end(data,'binary');
-                        });
-                        //responseMain.writeHead(200, {"Content-Type": 'text/plain'});
-                        //responseMain.end(date.getTime()+'');
+        };
+        
+        this.getImage = function() {
+            that.http.get(that.img.src, function(response) {
+                response.setEncoding('binary');
+                response.on('data', function(chunk){
+                    that.img.data += chunk;
+                });
+                
+                response.on('end',function() {
+                    that.img.contentType = response.headers['content-type'];
+                    that.fs.writeFile(that.img.new, that.img.data, {encoding: 'binary'}, function() {
+                        that.modImage();
                     });
                 });
+                
             });
-        });
-    });
-    server.listen(parseInt(port, 10));
+            return true;
+        };
+        
+        this.modImage = function() {
+            that.magick.convert([that.img.new, '-flip', that.img.tmp], function(err, stdout){
+                if (err) throw err;
+                that.fs.rename(that.img.tmp,that.img.new, function() {
+                    that.serveImage();
+                });
+            });
+        };
+        
+        this.serveImage = function() {
+            that.fs.readFile(that.img.new, function(err,data) {
+                if (err) throw err;
+                that.response.writeHead(200, {"Content-Type": that.img.contentType});
+                that.response.end(data,'binary');
+            });
+        };        
+    };
+    
+    var udt = new upside_down_ternet();
 }());
